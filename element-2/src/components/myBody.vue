f96c8a9187bd2dc9e98d4f0cb28000f84ffcb919
Christopher andrio <christopher_andrio@hotmail.com>
2017-10-26 13:54:25 -0200

    component myBody

--- element-2/src/components/myBody.vue
+++ element-2/src/components/myBody.vue
@@ -9,18 +9,57 @@
     }
 </script>
 <template>
-  <div>
-    <slot class="myContainer"></slot>
-  </div>
+  <el-container>
+    <slot name="aside"></slot>
+    <el-container class="myContainer">
+      <slot name="header"></slot>
+      <slot name="main"></slot>
+      <slot name="footer"></slot>
+      </el-container>
+    </el-container>
 </template>
 <style>
 .myContainer {
-  min-height: 100%;
-  max-height: 100%;
-  height: 100%;
   padding-left: 64px;
+}
+.myContainer {
+  padding-left: 64px;
+}
+
+html, body, #app, .el-container, #app > div {
+    min-height: 100%;
+    max-height: 100%;
+    height: 100%;
+    width: 100%;
+    min-width: 100%;
+    max-width: 100%;
+}
+
+.el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  z-index: 2;
   width: 100%;
   min-width: 100%;
   max-width: 100%;
+  height: 60px;
+  min-height: 60px;
+  max-height: 60px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  padding: 65px 20px 20px;
+  z-index: 1;
+  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
+  margin-bottom: 0px;
+  overflow: inherit;
+}
+body > .el-container {
+  margin-bottom: 40px;
+  min-height: 100%;
+  height: 100%;
 }
 </style>
--- element-2/src/example/HelloWorld.vue
+++ element-2/src/example/HelloWorld.vue
@@ -54,174 +54,118 @@
 
 </script>
 <template>
-    <el-container>
+  <myBody>
+    <myAside slot="aside" src="logo.png" title="Nifty"/>
+        <myHeader slot="header">
+          <div slot="menu1024">
+            <el-col :xs="12" :sm="12" :md="12" :lg="12">
+              <el-menu mode="horizontal">
+                <el-menu-item index="2">
+                  <span slot="title">Navigator Two</span>
+                </el-menu-item>
 
-      <myAside src="logo.png" title="Nifty"/>
+                <el-menu-item index="3">
+                  <span slot="title">Navigator Three</span>
+                </el-menu-item>
 
-      <el-container>
-
-        <myBody>
-
-          <myHeader>
-            <div slot="menu1024">
-              <el-col :xs="12" :sm="12" :md="12" :lg="12">
-                <el-menu mode="horizontal">
-                  <el-menu-item index="2">
-                    <span slot="title">Navigator Two</span>
-                  </el-menu-item>
-
-                  <el-menu-item index="3">
-                    <span slot="title">Navigator Three</span>
-                  </el-menu-item>
-
-                  <el-submenu index="4">
-                    <template slot="title">
-                      <span slot="title">Navigator One</span>
-                    </template>
-                    <el-menu-item-group>
-                      <span slot="title">Group One</span>
-                      <el-menu-item index="4-1">item one</el-menu-item>
-                      <el-menu-item index="4-2">item two</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="Group Two">
-                      <el-menu-item index="4-3">item three</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="4-4">
-                      <span slot="title">item four</span>
-                      <el-menu-item index="4-4-1">item one</el-menu-item>
-                    </el-submenu>
+                <el-submenu index="4">
+                  <template slot="title">
+                    <span slot="title">Navigator One</span>
+                  </template>
+                  <el-menu-item-group>
+                    <span slot="title">Group One</span>
+                    <el-menu-item index="4-1">item one</el-menu-item>
+                    <el-menu-item index="4-2">item two</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="Group Two">
+                    <el-menu-item index="4-3">item three</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="4-4">
+                    <span slot="title">item four</span>
+                    <el-menu-item index="4-4-1">item one</el-menu-item>
                   </el-submenu>
-                </el-menu>
-              </el-col>
+                </el-submenu>
+              </el-menu>
+            </el-col>
 
-              <el-col :xs="12" :sm="12" :md="12" :lg="12" class="text-align-end">
-                <a href="#">Texto sem icone</a>
-                <a href="#"><i class="el-icon-edit"></i>Texto com icone</a>
-                <el-dropdown>
+            <el-col :xs="12" :sm="12" :md="12" :lg="12" class="text-align-end">
+              <a href="#">Texto sem icone</a>
+              <a href="#"><i class="el-icon-edit"></i>Texto com icone</a>
+              <el-dropdown>
                       <span class="el-dropdown-link">
                         <i class="el-icon-more transform-i"></i>
                       </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>Action 8</el-dropdown-item>
-                    <el-dropdown-item>Action 9</el-dropdown-item>
-                    <el-dropdown-item>Action 10</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </div>
-          </myHeader>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>Action 8</el-dropdown-item>
+                  <el-dropdown-item>Action 9</el-dropdown-item>
+                  <el-dropdown-item>Action 10</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </div>
+        </myHeader>
+        <el-main slot="main">
 
-          <el-main>
+          <myModuleTitle title="Eu sou um titulo">
+            <el-button>Default</el-button>
+            <el-button size="medium">Medium</el-button>
+            <el-button size="small">Small</el-button>
+            <el-button size="mini">Mini</el-button>
+          </myModuleTitle>
 
-            <myModuleTitle title="Eu sou um titulo">
-              <el-button>Default</el-button>
-              <el-button size="medium">Medium</el-button>
-              <el-button size="small">Small</el-button>
-              <el-button size="mini">Mini</el-button>
-            </myModuleTitle>
+          <myContainer>
+            <el-collapse accordion>
+              <el-collapse-item title="Filtros" name="1">
+                <el-row :gutter="20">
+                  <el-col :span="12">
+                    <label>Nome </label>
+                    <el-input
+                    />
+                  </el-col>
+                  <el-col :span="12">
+                    <label>Sobrenome</label>
+                    <el-input
+                    />
+                  </el-col>
+                </el-row>
 
-            <myContainer>
-              <el-collapse accordion>
-                <el-collapse-item title="Filtros" name="1">
-                  <el-row :gutter="20">
-                    <el-col :span="12">
-                      <label>Nome </label>
-                      <el-input
-                      />
-                    </el-col>
-                    <el-col :span="12">
-                      <label>Sobrenome</label>
-                      <el-input
-                      />
-                    </el-col>
-                  </el-row>
+                <el-row :gutter="20">
+                  <el-col :span="12">
+                    <el-button size="medium" type="warning">pesquisar</el-button>
+                  </el-col>
+                </el-row>
+              </el-collapse-item>
+            </el-collapse>
+          </myContainer>
 
-                  <el-row :gutter="20">
-                    <el-col :span="12">
-                      <el-button size="medium" type="warning">pesquisar</el-button>
-                    </el-col>
-                  </el-row>
-                </el-collapse-item>
-              </el-collapse>
-            </myContainer>
+          <myContainer>
+            <el-table
+              :data="tableData"
+              style="width: 100%">
+              <el-table-column
+                prop="date"
+                label="Date"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="Name"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="address"
+                label="Address">
+              </el-table-column>
+            </el-table>
+          </myContainer>
 
-            <myContainer>
-              <el-table
-                :data="tableData"
-                style="width: 100%">
-                <el-table-column
-                  prop="date"
-                  label="Date"
-                  width="180">
-                </el-table-column>
-                <el-table-column
-                  prop="name"
-                  label="Name"
-                  width="180">
-                </el-table-column>
-                <el-table-column
-                  prop="address"
-                  label="Address">
-                </el-table-column>
-              </el-table>
-            </myContainer>
-
-          </el-main>
-
-          <el-footer>Footer</el-footer>
-
-        </myBody>
-
-      </el-container>
-
-    </el-container>
+        </el-main>
+        <el-footer slot="footer">Footer</el-footer>
+  </myBody>
 </template>
 <style>
 .padding-11{
   padding: 11px 0;
 }
-
-html, body, #app, .el-container, #app > div {
-    min-height: 100%;
-    max-height: 100%;
-    height: 100%;
-    width: 100%;
-    min-width: 100%;
-    max-width: 100%;
-}
-
-.el-footer {
-  background-color: #B3C0D1;
-  color: #333;
-  z-index: 2;
-  width: 100%;
-  min-width: 100%;
-  max-width: 100%;
-  height: 60px;
-  min-height: 60px;
-  max-height: 60px;
-}
-
-.el-main {
-  background-color: #E9EEF3;
-  padding: 65px 20px 20px;
-  z-index: 1;
-  width: 100%;
-  min-width: 100%;
-  max-width: 100%;
-  margin-bottom: 0px;
-  overflow: inherit;
-}
-body > .el-container {
-  margin-bottom: 40px;
-  min-height: 100%;
-  height: 100%;
-}
-
-
-
-
-
-
 
 </style>
