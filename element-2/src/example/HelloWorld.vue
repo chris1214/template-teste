dd414a80f63a040730702f5a533a5599bf039b00
Christopher andrio <christopher_andrio@hotmail.com>
2017-10-26 13:57:11 -0200

    Commit

--- element-2/src/example/HelloWorld.vue
+++ element-2/src/example/HelloWorld.vue
@@ -56,111 +56,114 @@
 <template>
   <myBody>
     <myAside slot="aside" src="logo.png" title="Nifty"/>
-        <myHeader slot="header">
-          <div slot="menu1024">
-            <el-col :xs="12" :sm="12" :md="12" :lg="12">
-              <el-menu mode="horizontal">
-                <el-menu-item index="2">
-                  <span slot="title">Navigator Two</span>
-                </el-menu-item>
 
-                <el-menu-item index="3">
-                  <span slot="title">Navigator Three</span>
-                </el-menu-item>
+    <myHeader slot="header">
+      <div slot="menu1024">
+        <el-col :xs="12" :sm="12" :md="12" :lg="12">
+          <el-menu mode="horizontal">
+            <el-menu-item index="2">
+              <span slot="title">Navigator Two</span>
+            </el-menu-item>
 
-                <el-submenu index="4">
-                  <template slot="title">
-                    <span slot="title">Navigator One</span>
-                  </template>
-                  <el-menu-item-group>
-                    <span slot="title">Group One</span>
-                    <el-menu-item index="4-1">item one</el-menu-item>
-                    <el-menu-item index="4-2">item two</el-menu-item>
-                  </el-menu-item-group>
-                  <el-menu-item-group title="Group Two">
-                    <el-menu-item index="4-3">item three</el-menu-item>
-                  </el-menu-item-group>
-                  <el-submenu index="4-4">
-                    <span slot="title">item four</span>
-                    <el-menu-item index="4-4-1">item one</el-menu-item>
-                  </el-submenu>
-                </el-submenu>
-              </el-menu>
-            </el-col>
+            <el-menu-item index="3">
+              <span slot="title">Navigator Three</span>
+            </el-menu-item>
 
-            <el-col :xs="12" :sm="12" :md="12" :lg="12" class="text-align-end">
-              <a href="#">Texto sem icone</a>
-              <a href="#"><i class="el-icon-edit"></i>Texto com icone</a>
-              <el-dropdown>
-                      <span class="el-dropdown-link">
-                        <i class="el-icon-more transform-i"></i>
-                      </span>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>Action 8</el-dropdown-item>
-                  <el-dropdown-item>Action 9</el-dropdown-item>
-                  <el-dropdown-item>Action 10</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </el-col>
-          </div>
-        </myHeader>
-        <el-main slot="main">
+            <el-submenu index="4">
+              <template slot="title">
+                <span slot="title">Navigator One</span>
+              </template>
+              <el-menu-item-group>
+                <span slot="title">Group One</span>
+                <el-menu-item index="4-1">item one</el-menu-item>
+                <el-menu-item index="4-2">item two</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group Two">
+                <el-menu-item index="4-3">item three</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="4-4">
+                <span slot="title">item four</span>
+                <el-menu-item index="4-4-1">item one</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-col>
 
-          <myModuleTitle title="Eu sou um titulo">
-            <el-button>Default</el-button>
-            <el-button size="medium">Medium</el-button>
-            <el-button size="small">Small</el-button>
-            <el-button size="mini">Mini</el-button>
-          </myModuleTitle>
+        <el-col :xs="12" :sm="12" :md="12" :lg="12" class="text-align-end">
+          <a href="#">Texto sem icone</a>
+          <a href="#"><i class="el-icon-edit"></i>Texto com icone</a>
+          <el-dropdown>
+                  <span class="el-dropdown-link">
+                    <i class="el-icon-more transform-i"></i>
+                  </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>Action 8</el-dropdown-item>
+              <el-dropdown-item>Action 9</el-dropdown-item>
+              <el-dropdown-item>Action 10</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </div>
+    </myHeader>
 
-          <myContainer>
-            <el-collapse accordion>
-              <el-collapse-item title="Filtros" name="1">
-                <el-row :gutter="20">
-                  <el-col :span="12">
-                    <label>Nome </label>
-                    <el-input
-                    />
-                  </el-col>
-                  <el-col :span="12">
-                    <label>Sobrenome</label>
-                    <el-input
-                    />
-                  </el-col>
-                </el-row>
+    <el-main slot="main">
 
-                <el-row :gutter="20">
-                  <el-col :span="12">
-                    <el-button size="medium" type="warning">pesquisar</el-button>
-                  </el-col>
-                </el-row>
-              </el-collapse-item>
-            </el-collapse>
-          </myContainer>
+      <myModuleTitle title="Eu sou um titulo">
+        <el-button>Default</el-button>
+        <el-button size="medium">Medium</el-button>
+        <el-button size="small">Small</el-button>
+        <el-button size="mini">Mini</el-button>
+      </myModuleTitle>
 
-          <myContainer>
-            <el-table
-              :data="tableData"
-              style="width: 100%">
-              <el-table-column
-                prop="date"
-                label="Date"
-                width="180">
-              </el-table-column>
-              <el-table-column
-                prop="name"
-                label="Name"
-                width="180">
-              </el-table-column>
-              <el-table-column
-                prop="address"
-                label="Address">
-              </el-table-column>
-            </el-table>
-          </myContainer>
+      <myContainer>
+        <el-collapse accordion>
+          <el-collapse-item title="Filtros" name="1">
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <label>Nome </label>
+                <el-input
+                />
+              </el-col>
+              <el-col :span="12">
+                <label>Sobrenome</label>
+                <el-input
+                />
+              </el-col>
+            </el-row>
 
-        </el-main>
-        <el-footer slot="footer">Footer</el-footer>
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-button size="medium" type="warning">pesquisar</el-button>
+              </el-col>
+            </el-row>
+          </el-collapse-item>
+        </el-collapse>
+      </myContainer>
+
+      <myContainer>
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="Date"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="Name"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="Address">
+          </el-table-column>
+        </el-table>
+      </myContainer>
+
+    </el-main>
+
+    <el-footer slot="footer">Footer</el-footer>
   </myBody>
 </template>
 <style>
